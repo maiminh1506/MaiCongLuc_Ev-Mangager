826b9d73a6e01812cbd576f65a80b40a281682d4
maiminh1506 <91531330+maiminh1506@users.noreply.github.com>
2024-11-14 13:12:54 +0700

    Add folder

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,44 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Event Manager';
+
+  @override
+  String get addEvent => 'Add Event';
+
+  @override
+  String get eventDetail => 'Detail Event';
+
+  @override
+  String get day => 'Day';
+
+  @override
+  String get week => 'Week';
+
+  @override
+  String get workWeek => 'Work Week';
+
+  @override
+  String get month => 'Month';
+
+  @override
+  String get timelineDay => 'Timeline Day';
+
+  @override
+  String get timelineWeek => 'Timeline Week';
+
+  @override
+  String get timelineWorkWeek => 'Timeline Work Week';
+
+  @override
+  String get timelineMonth => 'Timeline Month';
+
+  @override
+  String get schedule => 'Schedule';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_vi.dart
@@ -0,0 +1,44 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get appTitle => 'Quản lý sự kiện';
+
+  @override
+  String get addEvent => 'Thêm sự kiện';
+
+  @override
+  String get eventDetail => 'Chi tiết sự kiện';
+
+  @override
+  String get day => 'Ngày';
+
+  @override
+  String get week => 'Tuần';
+
+  @override
+  String get workWeek => 'Ngày Làm Việc';
+
+  @override
+  String get month => 'Tháng';
+
+  @override
+  String get timelineDay => 'Timeline Day';
+
+  @override
+  String get timelineWeek => 'Timeline Week';
+
+  @override
+  String get timelineWorkWeek => 'Timeline Work Week';
+
+  @override
+  String get timelineMonth => 'Timeline Month';
+
+  @override
+  String get schedule => 'Lịch Trình';
+}
